eef3deb626ed79d03a44c8cc2e90c3567f2f8fc1
abi <abysemail@zohomail.in>
2026-06-03 12:26:49 +0530

    Create frida_down package directory with __init__.py

---
+++ frida_down/__init__.py
@@ -0,0 +1,8 @@
+"""Frida binaries downloader package."""
+
+__version__ = "0.1.0"
+__author__ = "its-me-abi"
+__email__ = "keralaboypypi@keralaboy.anonaddy.com"
+__license__ = "AGPL-3.0"
+
+__all__ = []
